a38147b3e6a2f2fc40e3258f804d1c76d11d0b31
Eddwiin <maguran.mahendrarajan@gmail.com>
2022-03-28 00:48:34 +0200

    add condition when question is finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import './answer.dart';
+import './question.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'Who\'s is your favorite instructor?',
+      'answers': ['Max', 'Max', 'Max', 'Max'],
+    },
+  ];
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: _questionIndex < questions.length
+              ? Column(children: [
+                  Question(questions[_questionIndex]['questionText']),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ])
+              : Center(
+                  child: Text('You did it'),
+                )),
+    );
   }
 }
